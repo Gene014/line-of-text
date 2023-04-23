a5592f72447fc47ee8e2f887038a7c447a472f75
Gene014 <calmaeugene06@gmail.com>
2023-04-24 00:01:07 +0800

    encoding the output to txtfile, running

--- programcode.py
+++ programcode.py
@@ -2,24 +2,13 @@
 # BSCPE_1-4
 # # OOP Laboratory Exercise Number 3 Problem 2 (LINE-OF-TEXT)
 
-#Write a method in python to write multiple line of text contents into a text file mylife.txt. See sample output:
-def write_to_file():
- # OPEN file 'mylife.txt' in write mode
-  with open('mylife.txt', 'w') as file:
-  # SET line equal to user input of a line of text
-     line = input("Enter line: ")
-  # WRITE line to file with newline character
-     file.write(line + '\n')
-  # SET line_choice equal to user input of 'y' or 'n'
-     line_choice = input("Enter another line? (y/n): ")
-  # WHILE line_choice.lower is equal to 'y'
-  while line_choice.lower() == 'y':
-    # SET line equal to user input of a line of text
-      line = input("Enter line: ")
-    # WRITE line to file with newline character
-      file.write(line + '\n')
-    # SET line_choice equal to user input of 'y' or 'n'
-      line_choice = input("Enter another line? (y/n): ")
-  # CLOSE file
-  file.close()
-  print("Writing to file complete")
+#Write a method in python to write multiple line of text contents into a text file mylife.txt.
+def process():
+        # open numbers.txt (read), double txt even.txt (append), triple odd.txt (append)
+    with open("mylife.txt","a") as input_file:
+        user_input= input("Good day! Please type your line of text!")
+        # encode the user input to the txt file
+        input_file.write(user_input + "\n")
+
+process()
+        
