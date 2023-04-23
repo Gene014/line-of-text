66b669170f5255e149677291536056f34baba6cf
Gene014 <calmaeugene06@gmail.com>
2023-04-24 00:25:30 +0800

    insert looping. code running but overwriting  the statements in txt.file

--- programcode.py
+++ programcode.py
@@ -4,11 +4,18 @@
 
 #Write a method in python to write multiple line of text contents into a text file mylife.txt.
 def process():
-        # open numbers.txt (read), double txt even.txt (append), triple odd.txt (append)
+        # open mylife.txt(append)
     with open("mylife.txt","a") as input_file:
         user_input= input("Good day! Please type your line of text!")
-        # encode the user input to the txt file
+        #ask the user to try and input once again
+        askyesno = input("\n\33[36mDo you still want to continue? (yes/no):\33[0m ")
+        if askyesno.lower() == 'yes':
+            user_input=input("Please type your new line of text")
+        else: 
+            askyesno.lower() == 'no'
+            print("\33[41mTerminating Program... Your text is generated, feel free to open it at mylife.txt. Have a good day!")
+            exit()
+            # encode the user input to the txt file
         input_file.write(user_input + "\n")
-
 process()
         
