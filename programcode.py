06da4a8193cfeceea8bd957119bc9284eef1ea82
Gene014 <calmaeugene06@gmail.com>
2023-04-24 01:22:00 +0800

    finishing touches

--- programcode.py
+++ programcode.py
@@ -1,13 +1,13 @@
 # Calma, Eugene Marie S.
 # BSCPE_1-4
-# # OOP Laboratory Exercise Number 3 Problem 2 (LINE-OF-TEXT)
+# # OOP Laboratory Exercise Number 3 Problem 3 (LINE-OF-TEXT)
 
 #Write a method in python to write multiple line of text contents into a text file mylife.txt.
 def process():
     while True:
             # open mylife.txt(append)
         with open("mylife.txt","a") as input_file:
-            user_input= input("Good day! Please type your line of text!")
+            user_input= input("\n\33[33mGood day! Please type your line of text!")
             # encode the user input to the txt file
             input_file.write(user_input + "\n")
             #ask the user to try and input once again
@@ -18,4 +18,3 @@ def process():
             print("\33[41mTerminating Program... Your text is generated, feel free to open it at mylife.txt. Have a good day!")
             exit()          
 process()
-        
